3a72ea9e5f53a64a5e8affd2bda0434468ca13d6
MarcoFagioli <coich1996@gmail.com>
2018-01-28 14:56:20 +0100

    Easier approach, not dynamic. Figure out how solve space not used issue

--- additive-matrix.c
+++ additive-matrix.c
@@ -7,17 +7,10 @@
 
 #include "additive-matrix.h"
 
-int** matrix_read(char *file, int n_max){
+int matrix_read(char *file, int matrix[N_MAX][N_MAX]){
 	
-	int i, j;
-	int *array, **valid_matrix;
-
-	// Allocate space for read value
-	array = malloc(pow(n_max, 2) * sizeof(int *));
-	if(array == NULL){
-		printf("Out of memory\n");
-		exit(-1);
-	}
+	int i=0, j=0;
+	char a;
 
 	// Open and read file
 	FILE *input_file;
@@ -28,50 +21,27 @@ int** matrix_read(char *file, int n_max){
 		exit(-1);
 	}
 	
-	int n=0;
-	fscanf(input_file, "%d", &n);
-	if(n>n_max)
-		exit(-1);
+	while(! feof(input_file)){
+		fscanf(input_file, "%d", &matrix[i][j]);
+		j++;
+		fscanf(input_file, "%c", &a);
+		if(a == '\n'){
+			i++;
+			j=0;
+		}	
+	}
+	i++;
 
-	for(i=0; i<pow(n_max, 2); i++)
-		fscanf(input_file, "%d", &array[i]);
-		
-	fflush(input_file);
 	fclose(input_file);
 
-	// Allocate space for valid matrix
-	valid_matrix = malloc(n * sizeof(int *));
-	if(valid_matrix == NULL){
-		printf("Out of memory\n");
-		exit(-1);
-	}
-	for(i=0; i<n; i++){
-		valid_matrix[i] = malloc(n * sizeof(int));
-		if(valid_matrix[i] == NULL){
-			printf("Out of memory\n");
-			exit(-1);
-		}
-	}
-
-	// Fill the matrix with only the valid values
-	int k=0;
-	for(i=0; i<n; i++)
-		for(j=0; j<n; j++){
-        	valid_matrix[i][j] = array[k];
-        	k++;
-		}
-    
-    //Free space used to the array, with extra values
-    free(array);
-
-	return valid_matrix;
+	return i;
 }
 
-bool simmetric(int **matrix){
+bool matrix_simmetric(int matrix[N_MAX][N_MAX], int matrix_dimension){
 	int i, j;
 
-	for(i=0; i<sizeof(matrix); i++)
-		for(j=i; j<sizeof(matrix); j++){
+	for(i=0; i<matrix_dimension; i++)
+		for(j=i; j<matrix_dimension; j++){
 			if((i==j)&&(matrix[i][j]!=0))
 				return false;
 			if((i!=j)&&(matrix[i][j]!=matrix[j][i]))
@@ -81,23 +51,19 @@ bool simmetric(int **matrix){
 	return true;
 }
 
-bool main(int argc, char *argv[]) {
+void main(int argc, char *argv[]) {
 	
-	int matrix_dimension = atoi(argv[2]);
-	int **valid_matrix = matrix_read(argv[1], matrix_dimension);
-
-	//Check if matrix is simmetric
-	bool simm = simmetric(valid_matrix);
-	if(simm == false)
-		return false;
-
-	//parte di stampa
 	int i, j;
-	for(i=0; i<sizeof(valid_matrix); i++){
-		for(j=0; j<sizeof(valid_matrix); j++)
-			printf("%d ", valid_matrix[i][j]);
+	int matrix[N_MAX][N_MAX];
+
+	int n_dimension = matrix_read(argv[1], matrix);
+	bool simmetric = matrix_simmetric(matrix, n_dimension);
+	printf("%d\n", simmetric);
+
+	for(i=0; i<n_dimension; i++){
+		for(j=0; j<n_dimension; j++)
+			printf("%d ", matrix[i][j]);
 		printf("\n");
 	}
 
-	return true;
 }
--- additive-matrix.h
+++ additive-matrix.h
@@ -6,6 +6,7 @@
   
 */
 
+#define N_MAX 50
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
