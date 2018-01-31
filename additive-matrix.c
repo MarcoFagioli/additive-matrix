9354b485e788ad6370c719762debaf00bbec3aa5
MarcoFagioli <coich1996@gmail.com>
2018-01-31 18:48:49 +0100

    New check on input. Added some matrix for testing and output file.

--- additive-matrix.c
+++ additive-matrix.c
@@ -7,17 +7,16 @@
 
 #include "additive-matrix.h"
 
-int matrix_read(char *file, int matrix[N_MAX][N_MAX]){
+int input_read(char *in_file, int matrix[N_MAX][N_MAX]){
 	
 	int i=0, j=0;
 	char a;
 
-	// Open and read file
 	FILE *input_file;
-	input_file = fopen(file, "r");
+	input_file = fopen(in_file, "r");
 	
 	if(input_file == NULL){
-		printf("Error in file read");
+		printf("Error in input file open");
 		exit(-1);
 	}
 	
@@ -30,11 +29,21 @@ int matrix_read(char *file, int matrix[N_MAX][N_MAX]){
 			j=0;
 		}	
 	}
-	i++;
 
 	fclose(input_file);
+	return ++i;
+}
 
-	return i;
+bool matrix_positive(int matrix[N_MAX][N_MAX], int matrix_dimension){
+	
+	int i, j;
+
+	for(i=0; i<matrix_dimension; i++)
+		for(j=i; j<matrix_dimension; j++)
+			if(matrix[i][j]<0)
+				return false;
+
+	return true;
 }
 
 bool matrix_simmetric(int matrix[N_MAX][N_MAX], int matrix_dimension){
@@ -52,25 +61,28 @@ bool matrix_simmetric(int matrix[N_MAX][N_MAX], int matrix_dimension){
 	return true;
 }
 
-bool additive(int matrix[N_MAX][N_MAX], int n){
+bool matrix_additive(int matrix[N_MAX][N_MAX], int matrix_dimension){
 
 	int i, j, k, l;
 	int sum1, sum2, sum3;
 	bool four_condition;
 
-	for(i=0; i<n; i++)
-		for(j=i+1; j<n; j++)
-			for(k=j+1; k<n; k++)
-				for(l=k+1; l<n; l++){
+	for(i=0; i<matrix_dimension; i++)
+		for(j=i+1; j<matrix_dimension; j++)
+			for(k=j+1; k<matrix_dimension; k++)
+				for(l=k+1; l<matrix_dimension; l++){
 					four_condition = false;
-					printf("Point (%d,%d,%d,%d)\n", i,j,k,l);
+					#ifdef DEBUG
+						printf("Point (%d,%d,%d,%d)\n", i,j,k,l);
+					#endif
 					sum1 = matrix[i][j] + matrix[k][l];
 					sum2 = matrix[i][k] + matrix[j][l];
 					sum3 = matrix[i][l] + matrix[j][k];
-					printf("(%d,%d) (%d,%d) %d + %d Sum 1 = %d\n", i,j,k,l, matrix[i][j], matrix[k][l], sum1);
-					printf("(%d,%d) (%d,%d) %d + %d Sum 2 = %d\n", i,k,j,l, matrix[i][k], matrix[j][l], sum2);
-					printf("(%d,%d) (%d,%d) %d + %d Sum 3 = %d\n\n", i,l,j,k, matrix[i][l], matrix[j][k], sum3);
-
+					#ifdef DEBUG
+						printf("(%d,%d) (%d,%d) %d + %d Sum 1 = %d\n", i,j,k,l, matrix[i][j], matrix[k][l], sum1);
+						printf("(%d,%d) (%d,%d) %d + %d Sum 2 = %d\n", i,k,j,l, matrix[i][k], matrix[j][l], sum2);
+						printf("(%d,%d) (%d,%d) %d + %d Sum 3 = %d\n\n", i,l,j,k, matrix[i][l], matrix[j][k], sum3);
+					#endif
 
 					if(sum1 < sum2)
 						if(sum2 == sum3)
@@ -89,7 +101,41 @@ bool additive(int matrix[N_MAX][N_MAX], int n){
 				}
 
 	return true;
+}
 
+void output_print(int matrix[N_MAX][N_MAX], int matrix_dimension){
+
+	int i, j;
+	FILE *output_file;
+	output_file = fopen("result.txt", "w");
+	
+	if(output_file == NULL){
+		printf("Error in output file open");
+		exit(-1);
+	}
+
+	for(i=0; i<matrix_dimension; i++){
+		for(j=0; j<matrix_dimension; j++)
+			fprintf(output_file, "%d\t", matrix[i][j]);
+		fprintf(output_file, "\n");
+	}
+
+	fclose(output_file);
+}
+
+void output_error(char *error){
+
+	FILE *output_file;
+	output_file = fopen("result.txt", "w");
+	
+	if(output_file == NULL){
+		printf("Error in output file open");
+		exit(-1);
+	}
+	
+	fprintf(output_file, "%s", error);
+
+	fclose(output_file);
 }
 
 void main(int argc, char *argv[]) {
@@ -97,17 +143,34 @@ void main(int argc, char *argv[]) {
 	int i, j;
 	int matrix[N_MAX][N_MAX];
 
-	int n_dimension = matrix_read(argv[1], matrix);
-	bool simmetric = matrix_simmetric(matrix, n_dimension);
-	printf("Simm = %d\n", simmetric);
+	int matrix_dimension = input_read(argv[1], matrix);
 
-	bool add = additive(matrix, n_dimension);
-	printf("Add = %d\n", add);
-
-	for(i=0; i<n_dimension; i++){
-		for(j=0; j<n_dimension; j++)
-			printf("%d\t", matrix[i][j]);
-		printf("\n");
+	bool positive = matrix_positive(matrix, matrix_dimension);
+	#ifdef DEBUG
+		printf("Positive = %d\n", positive);
+	#endif
+	if(positive!=true){
+		output_error("Some element of the matrix is not positive.\nInput matrix is not valid.");
+		exit(-1);
 	}
 
+	bool simmetric = matrix_simmetric(matrix, matrix_dimension);
+	#ifdef DEBUG
+		printf("Simmetric = %d\n", simmetric);
+	#endif
+	if(simmetric!=true){
+		output_error("The matrix is not simmetric.\nThe matrix is not additive.");
+		exit(-1);
+	}
+
+	bool additive = matrix_additive(matrix, matrix_dimension);
+	#ifdef DEBUG
+		printf("Additive = %d\n", additive);
+	#endif
+	if(additive!=true){
+		output_error("The matrix is not additive.");
+		exit(-1);
+	}
+
+	output_print(matrix, matrix_dimension);
 }
--- additive-matrix.h
+++ additive-matrix.h
@@ -6,6 +6,8 @@
   
 */
 
+#define DEBUG
+
 #define N_MAX 50
 #include <stdlib.h>
 #include <stdio.h>
